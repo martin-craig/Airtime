af6cafc30e32d56f1d01e5d840b2dc0c49e83878
Albert Santoni <albert.santoni@sourcefabric.org>
2013-10-30 18:07:31 -0400

    Added API key auth using HTTP basic auth

--- airtime_mvc/application/modules/rest/controllers/ShowController.php
+++ airtime_mvc/application/modules/rest/controllers/ShowController.php
@@ -21,41 +21,144 @@ class Rest_ShowController extends Zend_Rest_Controller
 
     public function indexAction()
     {
+        if (!$this->verifyApiKey()) {
+            return;
+        }
          $this->getResponse()
-            ->appendBody("From indexAction() returning all articles");
+            ->appendBody("From indexAction() returning all shows");
     }
     public function getAction()
     {
-        //$this->getResponse()
-        //    ->appendBody("From getAction() returning the requested article");
+        if (!$this->verifyApiKey()) {
+            return;
+        }
+        $id = $this->getId();
+        if (!$id) {
+            return;
+        }
 
-        if (!$id = $this->_getParam('id', false)) {
-                // report error, redirect, etc.
-        } 
-
-        $query = Airtime\CcShowQuery::create();
-        $show = $query->findPk($id);
-
-        Logging::info($show); 
-        $this->getResponse()
-            ->appendBody($show->exportTo("JSON"));
+        $show = Airtime\CcShowQuery::create()->findPk($id);
+        if ($show) {
+            $this->getResponse()
+                ->appendBody($show->exportTo("JSON"));
+        } else {
+            $this->showNotFoundResponse();
+        }
     }
     
     public function postAction()
     {
-        $this->getResponse()
-            ->appendBody("From postAction() creating the requested article");
+        if (!$this->verifyApiKey()) {
+            return;
+        }
+        //If we do get an ID on a POST, then that doesn't make any sense
+        //since POST is only for creating.
+        if ($id = $this->_getParam('id', false)) {
+            $resp = $this->getResponse();
+            $resp->setHttpResponseCode(400);
+            $resp->appendBody("ERROR: ID should not be specified when using POST. POST is only used for show creation, and an ID will be chosen by Airtime"); 
+            return;
+        }
+
+        $show = new Airtime\CcShow(); 
+        $rawRequestBody = $this->getRequest()->getRawBody();
+        //Hacky check to see if the request is a whole JSON object (updating a Show)
+        if ($rawRequestBody[0] == '{') {
+            $show->importFrom('JSON', $rawRequestBody);
+            $show->save();
+            return;
+        }
+
+        //Otherwise, we're assuming this is a request to create a new show, using
+        //URL parameters passed to us.
+        //
+        //TODO: Implement creation!!
+        //TODO: Create a show
+        //TODO: Then create a show instance?
+        //if ($id = $this->_getParam('id', false)) {
+        //stat
     }
     
     public function putAction()
     {
-        $this->getResponse()
-            ->appendBody("From putAction() updating the requested article");
+        if (!$this->verifyApiKey()) {
+            return;
+        }
+        $id = $this->getId();
+        if (!$id) {
+            return;
+        }
+        
+        $show = Airtime\CcShowQuery::create()->findPk($id);
+        if ($show)
+        {
+            $show->importFrom('JSON', $this->getRequest()->getRawBody());
+            $show->save();
+            $this->getResponse()
+                ->appendBody("From putAction() updating the requested show");
+        } else {
+            $this->showNotFoundResponse();
+        }
     }
     
     public function deleteAction()
     {
-        $this->getResponse()
-            ->appendBody("From deleteAction() deleting the requested article");
+        if (!$this->verifyApiKey()) {
+            return;
+        }
+        $id = $this->getId();
+        if (!$id) {
+            return;
+        }
+        $show = Airtime\CcShowQuery::create()->$query->findPk($id);
+        if ($show) {
+            $show->delete();
+        } else {
+            $this->showNotFoundResponse();
+        }
+    }
+
+    private function getId()
+    {
+        if (!$id = $this->_getParam('id', false)) {
+            $resp = $this->getResponse();
+            $resp->setHttpResponseCode(400);
+            $resp->appendBody("ERROR: No show ID specified."); 
+            return false;
+        } 
+        return $id;
+    }
+
+    private function verifyAPIKey()
+    {
+        //The API key is passed in via HTTP "basic authentication":
+        //  http://en.wikipedia.org/wiki/Basic_access_authentication
+
+        //TODO: Fetch the user's API key from the database to check against 
+        $unencodedStoredApiKey = "foobar"; 
+        $encodedStoredApiKey = base64_encode($unencodedStoredApiKey . ":");
+
+        //Decode the API key that was passed to us in the HTTP request.
+        $authHeader = $this->getRequest()->getHeader("Authorization");
+        $encodedRequestApiKey = substr($authHeader, strlen("Basic "));
+
+        if ($encodedRequestApiKey === $encodedStoredApiKey)
+        {
+            return true;
+        }
+        else
+        {
+            $resp = $this->getResponse();
+            $resp->setHttpResponseCode(401);
+            $resp->appendBody("ERROR: Incorrect API key."); 
+            return false;
+        }
+    }
+
+    private function showNotFoundResponse()
+    {
+        $resp = $this->getResponse();
+        $resp->setHttpResponseCode(404);
+        $resp->appendBody("ERROR: Show not found."); 
     }
 }
